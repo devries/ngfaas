4d6dbef04bcf657c8793d1dc3303d960a0f5a3cd
Christopher De Vries <devries@idolstarastronomer.com>
2019-11-09 11:28:58 -0500

    Added server interceptor to check token

--- server_four/main.go
+++ server_four/main.go
@@ -55,16 +55,35 @@ func main() {
 		ClientCAs:    certPool,
 	}
 
-	s := grpc.NewServer(grpc.Creds(credentials.NewTLS(tlsConfig)))
+	s := grpc.NewServer(grpc.Creds(credentials.NewTLS(tlsConfig)), grpc.UnaryInterceptor(AuthenticationInterceptor))
 	api.RegisterNgFaaSServer(s, &server{})
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("Failed to serve: %s", err)
 	}
 }
 
-type server struct{}
+// Let's create a Context Key type with a String() method
+type contextKey string
 
-func (s *server) GetFucks(ctx context.Context, in *api.FuckNumber) (*api.FuckBox, error) {
+func (c contextKey) String() string {
+	return "grpc server context key " + string(c)
+}
+
+var contextKeyAuthorized = contextKey("authorized")
+
+// This function returns authorized from the context
+func Authorized(ctx context.Context) bool {
+	authorized, ok := ctx.Value(contextKeyAuthorized).(bool)
+	if !ok {
+		return false
+	}
+
+	return authorized
+}
+
+// This is a UnaryServerInterceptor type which is a function with the signature below.
+func AuthenticationInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
+	// We can perform a simple logging function here, getting the client address.
 	p, ok := peer.FromContext(ctx)
 	if ok {
 		log.Printf("Received Request from %s", p.Addr)
@@ -72,22 +91,44 @@ func (s *server) GetFucks(ctx context.Context, in *api.FuckNumber) (*api.FuckBox
 		log.Printf("Received Request")
 	}
 
-	// In this case we will read the token directly from the context, but it makes sense
-	// to use an Interceptor to validate the token and potentially add role information
-	// to the context for use by the service functions.
+	// We will get the token from the context
 	md, ok := metadata.FromIncomingContext(ctx)
+	authorized := false
 	if !ok {
 		log.Printf("Received no metadata")
-		return nil, status.Errorf(codes.Unauthenticated, "Unable to access metadata")
+		authorized = false
 	} else {
 		tokens := md.Get("authorization")
 		if len(tokens) < 1 || tokens[0] != "Bearer HelloWorld" {
 			log.Printf("Invalid or Missing token")
-			return nil, status.Errorf(codes.Unauthenticated, "Invalid or missing authorization token")
+			authorized = false
+		} else {
+			log.Printf("Received valid authorization token")
+			authorized = true
 		}
-		log.Printf("Received valid authorization token")
 	}
 
+	ctx = context.WithValue(ctx, contextKeyAuthorized, authorized)
+
+	h, err := handler(ctx, req)
+	return h, err
+}
+
+type server struct{}
+
+func (s *server) GetFucks(ctx context.Context, in *api.FuckNumber) (*api.FuckBox, error) {
+	// The interceptor places a boolean in the context to let us know if the client is authorized.
+	authorized := Authorized(ctx)
+
+	// Next we check to see if it is false.
+	if !authorized {
+		log.Printf("Unauthorized client")
+		return nil, status.Errorf(codes.Unauthenticated, "Invalid or missing authorization token")
+	}
+	log.Printf("Authorized client")
+
+	// Finally we handle the logic of the server, checking if the number of fucks requested is between
+	// 0 and 500 (inclusive), and returning the requested number.
 	if in.Number < 0 {
 		retErr := status.Errorf(codes.InvalidArgument, "Negative fucks are not allowed")
 		log.Printf("Error: Asked for negative fucks")
@@ -99,6 +140,8 @@ func (s *server) GetFucks(ctx context.Context, in *api.FuckNumber) (*api.FuckBox
 		log.Printf("Error: Asked for too many fucks")
 		return nil, retErr
 	}
+
+	log.Printf("Returning %d fucks", in.Number)
 
 	contentBox := make([]string, in.Number)
 
